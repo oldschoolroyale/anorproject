0199663bb076c886a7755f5f1dbce4b32709d8cb
AdminNote <oldschool.royale@gmail.com>
2021-05-18 12:23:11 +0500

    Packages added

--- app/src/main/java/com/brmlab/anorproject/ui/activity/MainActivity.kt
+++ app/src/main/java/com/brmlab/anorproject/ui/activity/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.brmlab.anorproject
+package com.brmlab.anorproject.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.brmlab.anorproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
